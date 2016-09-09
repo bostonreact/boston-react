436e2ffaef434118313309bafa4ff525c4fd40a0
Dan Pickett <dan.pickett@launchacademy.com>
2016-09-09 16:14:47 -0400

    shed some boilerplate, initial site

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
+import LogoImage from '../assets/react-logo.png'
 import classes from './HomeView.scss'
 
 export const HomeView = () => (
   <div>
-    <h4>Welcome!</h4>
     <img
-      alt='This is a duck, because Redux!'
-      className={classes.duck}
-      src={DuckImage} />
+      alt="ReactJS Logo"
+      className="react-logo"
+      src={LogoImage} />
+    <h1>Boston ReactJS</h1>
   </div>
 )
 
